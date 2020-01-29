86ca8fe57f59f1cc0fca31596e3dc788f4312aab
Alex CLIPOTA <ext_alexandruc@playtika.com>
2020-01-29 10:12:21 +0200

    Added react redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,14 @@
-import {
-    createStore,
-    applyMiddleware
-} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // redux-logger is a middleware that lets you log every state change
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 // redux-thunk is a middleware that lets you dispatch async actions
-// import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-// const middleware = applyMiddleware(thunk, logger);
-const middleware = applyMiddleware();
-const store = createStore(rootReducer, {}, middleware);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
 
 export default store;
